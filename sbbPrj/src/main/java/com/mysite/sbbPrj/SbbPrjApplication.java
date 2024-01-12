b6d88ccbe10111e06d9a08db9234b6b314dbc6fc
Sunn88 <139128459+Sunn88@users.noreply.github.com>
2024-01-12 16:05:12 +0900

    240112

---
+++ sbbPrj/src/main/java/com/mysite/sbbPrj/SbbPrjApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.sbbPrj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbbPrjApplication.class, args);
+	}
+
+}
---
+++ sbbPrj/src/test/java/com/mysite/sbbPrj/SbbPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sbbPrj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
