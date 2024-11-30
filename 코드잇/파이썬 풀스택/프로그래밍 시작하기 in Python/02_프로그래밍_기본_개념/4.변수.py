47d7d7b0be905dfb42878b35640c140a565ef527
Sunn88 <139128459+Sunn88@users.noreply.github.com>
2024-12-01 00:07:48 +0900

    241130

---
+++ 코드잇/파이썬 풀스택/프로그래밍 시작하기 in Python/02_프로그래밍_기본_개념/4.변수.py
@@ -0,0 +1,17 @@
+print(4990)
+print(4990 * 2)
+print(4990 + 1490)
+print(4990 * 3 + 1490 * 2 + 1250 * 5)
+#매번 일일히 입력해야 함 -> 실수 유발
+#이해하기 어려움
+#고치기 어려움
+
+#변수 : 데이터를 공간에 담아 이름표를 붙여줌
+burger_price = 4990 #정의
+fries_price = 1490
+drink_price = 1250
+
+print(burger_price)
+print(burger_price * 2)
+print(burger_price + fries_price)
+print(burger_price * 3 + fries_price * 2 + drink_price * 5)
---
+++ 코드잇/파이썬 풀스택/프로그래밍 시작하기 in Python/02_프로그래밍_기본_개념/5.함수.py
@@ -0,0 +1,9 @@
+burger_price = 4990
+print(burger_price)
+
+#함수 정의
+def hello(): 
+    print("Hello!")
+    print("Welcome to Codeit!")
+
+hello()
