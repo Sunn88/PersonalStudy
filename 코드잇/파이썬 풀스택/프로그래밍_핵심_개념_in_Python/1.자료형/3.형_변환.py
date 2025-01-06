7b2559f5c5a3b85728143af720ed3aeef172c6bb
Sunn88 <139128459+Sunn88@users.noreply.github.com>
2025-01-06 21:26:02 +0900

    250106

---
+++ 코드잇/파이썬 풀스택/프로그래밍_핵심_개념_in_Python/1.자료형/3.형_변환.py
@@ -0,0 +1,10 @@
+#형 변환(Type Casting) : 값을 한 자료형에서 다른 자료형으로 바꾸는 것
+
+print(int("2") + int("5"))
+print(float("1.1") + float("2.5"))
+print(str(2) + str(5))
+
+age = 7
+print("제 나이는 " + str(age) + "살입니다.")
+
+#print(int("Hello world!"))
