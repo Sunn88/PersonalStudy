550625da7564f998eca7ae63726dda3246a27457
Sunn88 <139128459+Sunn88@users.noreply.github.com>
2025-01-10 22:04:49 +0900

    250110

---
+++ 코드잇/파이썬 풀스택/프로그래밍_핵심_개념_in_Python/1.자료형/8.type_함수.py
@@ -0,0 +1,13 @@
+#어떤 자료형인지 확실치 않을 때 사용
+
+print(type(3))
+print(type(3.0))
+print(type("3"))
+
+print(type("True"))
+print(type(True))
+
+def hello():
+    print("Hello world!")
+print(type(hello))
+print(type(print))
