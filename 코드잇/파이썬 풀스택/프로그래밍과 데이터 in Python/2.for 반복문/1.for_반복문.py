f55070fd1bd5d3fc7987e49443d3b1613cab926e
Sunn88 <139128459+Sunn88@users.noreply.github.com>
2025-01-24 00:12:55 +0900

    250124

---
+++ 코드잇/파이썬 풀스택/프로그래밍과 데이터 in Python/2.for 반복문/1.for_반복문.py
@@ -0,0 +1,12 @@
+my_list = [2, 3, 5, 7, 11]
+
+# for 반복문 : 조건식이 없음
+for number in my_list:
+    print(number)
+
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
+
+# 상황에 따라 더 좋은 것 고르기
