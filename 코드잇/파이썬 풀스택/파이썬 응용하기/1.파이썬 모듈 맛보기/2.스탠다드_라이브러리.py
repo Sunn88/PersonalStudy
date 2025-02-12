326a8f12f42fb59459ec1ad88850e4893648aaaf
Sunn88 <139128459+Sunn88@users.noreply.github.com>
2025-02-12 23:29:35 +0900

    250212

---
+++ 코드잇/파이썬 풀스택/파이썬 응용하기/1.파이썬 모듈 맛보기/2.스탠다드_라이브러리.py
@@ -0,0 +1,22 @@
+# standard library(표준 라이브러리) : 자주 쓰는 모듈들
+
+# 수학 관련 기능들
+import math
+
+print(math.log10(100))
+print(math.cos(0))
+print(math.pi)
+
+
+#랜덤한 값 생성
+import random
+
+print(random.random())
+
+
+
+# 파이썬으로 운영체제 조작
+import os
+
+print(os.getlogin())
+print(os.getcwd())
