215a4a960d7ddde5a47dc9c266a70091ed656125
Sunn88 <139128459+Sunn88@users.noreply.github.com>
2025-02-26 23:18:27 +0900

    250226

---
+++ 코드잇/파이썬_풀스택/파이썬_응용하기/4.프로젝트_로또_시뮬레이션/4.로또_시물레이션_겹치는_번호_개수.py
@@ -0,0 +1,16 @@
+# main.py 파일의 count_matching_numbers() 함수를 작성하세요. 
+# 이 함수는 참가자가 뽑은 6개의 번호 리스트와 
+# 당첨 번호 6개의 리스트 중 몇 개의 숫자가 일치하는지 알려 주는 함수입니다. 
+# 파라미터로 리스트 numbers와 리스트 winning_numbers를 받고, 
+# 두 리스트 사이에 겹치는 번호 개수를 리턴합니다.
+
+def count_matching_numbers(list_1, list_2):
+    s1 = set(list_1)
+    s2 = set(list_2)
+    same_number = s1&s2
+    return len(same_number)
+
+
+# 테스트 코드
+print(count_matching_numbers([2, 7, 11, 14, 25, 40], [2, 11, 13, 14, 30, 35]))
+print(count_matching_numbers([2, 7, 11, 14, 25, 40], [14]))
